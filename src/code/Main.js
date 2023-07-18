72212da87520f069c3de02535e7def2ebf1f1cda
ShinJongjin <jong1598@douzone.com>
2023-07-18 11:00:31 +0900

    [초기세팅]

--- src/code/Main.js
+++ src/code/Main.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function Main() {
   return (
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +14,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
   );
 }
 
-export default App;
+export default Main;
